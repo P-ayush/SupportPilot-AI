b30fa83e182849fe7154168e31d47eedcda1598e
Ayush <ayushprasad481@gmail.com>
2026-06-01 15:35:38 +0530

    feat: refactor database connection setup and integrate Sequelize authentication in server startup

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,11 +1,16 @@
+import dotenv from "dotenv";
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(
-  process.env.DATABASE_URL as string,
-  {
+dotenv.config();
+
+const sequelize = new Sequelize({
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: 5432,
     dialect: "postgres",
     logging: false,
-  }
-);
+});
 
 export default sequelize;
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import routes from './routes';
+import sequelize from './config/database';
 
 dotenv.config();
 const app = express();
@@ -11,6 +12,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", routes);
 
-app.listen(3000, () => {
+app.listen(3000, async () => {
+    await sequelize.authenticate()
+    console.log('Database connected');
+    await sequelize.sync({ force: false });
+    console.log('Database synced');
     console.log('Server running on port 3000');
 });
