90bbc8720f4e529dde0f22d0e7695b9f0ac5c867
Ayush <ayushprasad481@gmail.com>
2026-06-03 14:46:27 +0530

    feat: update retrieveChunks function to support organization-based document retrieval and improve type safety

--- src/rag/retrieval.ts
+++ src/rag/retrieval.ts
@@ -1,11 +1,25 @@
-import { DocumentChunk } from "../models";
+import { DocumentChunk, Document } from "../models";
 import { embedQuery } from "./embeddings";
 import { cosineSimilarity } from "./similarity";
+import { IDocument } from "../types/document.types";
+import { Op } from "sequelize";
 
-export const retrieveChunks = async (question: string, documentId: number) => {
+export const retrieveChunks = async (question: string, organizationId: number) => {
     const queryEmbedding = await embedQuery(question);
-
-    const chunks = await DocumentChunk.findAll({ where: { document_id: documentId } });
+    const document = await Document.findAll({ where: { organization_id: organizationId } })
+    const documentIds = document.map(
+        doc => (doc.get() as IDocument).id
+    );
+    if (!documentIds.length) {
+        return [];
+    }
+    const chunks = await DocumentChunk.findAll({
+        where: {
+            document_id: {
+                [Op.in]: documentIds
+            }
+        }
+    });
     const scoredChunks = chunks.map(chunk => {
         const data = chunk.get() as any;
         return {
