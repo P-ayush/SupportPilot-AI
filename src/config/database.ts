8a0a5f06dfc7e91ba6c7d7d283879506d42488e7
Ayush <ayushprasad481@gmail.com>
2026-06-04 14:16:30 +0530

    feat: refactor database connection to use DB_URL for improved configuration and SSL support

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,14 +3,25 @@ import { Sequelize } from "sequelize";
 
 dotenv.config();
 
-const sequelize = new Sequelize({
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: 5432,
+// const sequelize = new Sequelize({
+//     database: process.env.DB_NAME,
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     host: process.env.DB_HOST,
+//     port: 5432,
+//     dialect: "postgres",
+//     logging: false,
+// });
+const sequelize = new Sequelize(process.env.DB_URL as string, {
     dialect: "postgres",
-    logging: false,
+    protocol: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
 });
+
 
 export default sequelize;
